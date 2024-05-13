40aae054a5ff105859dac282e700a58ec3bb4122
Tomykulak <xokruhli@mendelu.cz>
2024-05-13 18:38:56 +0200

    NavBar phone responsiveness finally works!

--- tomy-web/src/components/NavBar.vue
+++ tomy-web/src/components/NavBar.vue
@@ -4,21 +4,51 @@
     :elevation="20"
     app
     @click="onNavBarClickColorChange"
-    :color="state.navBarColor
-">
-    <v-responsive class="d-flex align-center text-center fill-height">
-      <v-row class=" ">
-        <v-col cols="auto">
+    :color="state.navBarColor"
+  >
+    <!-- Menu Button for small screens -->
+    <v-app-bar-nav-icon
+      @click="state.drawer = !state.drawer"
+      v-if="$vuetify.display.smAndDown"
+    >
+    </v-app-bar-nav-icon>
+    <v-navigation-drawer
+      class="my-7 pa-1"
+      color="primary"
+      v-if="!state.drawer"
+      v-model="state.drawer"
+      :location="$vuetify.display.smAndDown ? 'bottom' : undefined"
+      temporary
+    >
+      <v-list class="">
+        <v-list-item
+          class="navButton"
+          :class="{ active: $route.path === button.route }"
+          v-for="(button, index) in navigationButtons"
+          :key="index"
+          @click="onMenuItemClicked(button.route)"
+        >
+          <v-list-item-title>{{ button.name }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
+    <!-- Links for large screens -->
+    <v-responsive class="
+    d-flex
+     align-center
+      text-center
+       fill-height" v-if="!$vuetify.display.smAndDown">
+      <v-row>
+        <v-col cols="auto">
           <router-link
             v-for="(button, index) in navigationButtons"
             :key="index"
-            :to=button.route
-            v-slot="{ isActive }"
+            :to="button.route"
           >
             <v-btn
               class="navButton mx-2"
-              :class="{ active: isActive}"
+              :class="{ active: $route.path === button.route }"
               variant="outlined"
             >
               {{ button.name }}
@@ -31,42 +61,35 @@
 </template>
 
 <script lang="ts" setup>
-import Logo from '@/assets/logo.png';
+import { reactive, ref } from 'vue';
 import router from "@/router";
-import {reactive} from "vue";
 
-// List names and links for navBar
+
+
 const navigationButtons = [
   { name: "Home", route: '/' },
   { name: "About Me", route: '/about-me' },
   { name: "Projects", route: '/projects' },
   { name: "Game", route: '/game' },
-
-  // TODO finish buttons
-  /*
-  { name: "Game", route: '/game' }
-   */
 ];
-
 
 const state = reactive({
   navBarColor: "primary",
+  drawer: true,
   counter: 0
 });
 
-const onMenuClick = () => {
-      console.log('Menu button clicked');
-      router.push("/");
-    }
 const onNavBarClickColorChange = () => {
   state.counter++;
-  if (state.counter % 2 == 0) {
-    state.navBarColor = "primary"
-  } else {
-    state.navBarColor = "darkPurple3"
-  }
-}
+  state.navBarColor = state.counter % 2 === 0 ? "primary" : "darkPurple3";
+};
+
+const onMenuItemClicked = (path) => {
+  router.push(path);
+  state.drawer = true; // Close the drawer when an item is clicked
+};
 </script>
+
 
 
 <style scoped>
